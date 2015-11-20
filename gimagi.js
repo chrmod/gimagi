bcf0804e408e4f9a12f8c63633fdc19e9599e336
Sam Macbeth <sam@cliqz.com>
2015-11-20 11:16:54 +0100

    Split meetings by pending on.

--- gimagi.js
+++ gimagi.js
@@ -221,13 +221,15 @@ if (Meteor.isClient) {
       return Meetings.find().fetch().length;
     },
     pendingMeetings() {
+      var me = Meteor.user().profile.name;
       return Meetings.find({people: Meteor.user().profile.name}).fetch().filter(function (meeting) {
-        return meeting.status === "pending"
+        return meeting.status === "pending" && meeting.pendingon.indexOf(me) > -1;
       });
     },
     meetings: function () {
+      var me = Meteor.user().profile.name;
       return Meetings.find({people: Meteor.user().profile.name}).fetch().filter(function (meeting) {
-        return meeting.status !== "pending"
+        return meeting.pendingon.indexOf(me) == -1;
       });
     },
     detailsMode: function () {
